580c45a919313928095e45108fedce16e4205c02
Janusz Grabis <janusz@rsi.pl>
2014-04-17 18:22:23 +0200

    wildfly:default recipe

--- wildfly/metadata.rb
+++ wildfly/metadata.rb
@@ -1,6 +1,9 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "freelancer"
+maintainer_email "janusz@rsi.pl"
 license          "All rights reserved"
 description      "Installs/Configures wildfly"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.1
+
+recipe "wildfly::default", "Installs WildFly
+
