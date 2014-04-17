8f5447c1afb110e7f2922f2a5a616fb161c95933
Janusz Grabis <janusz@rsi.pl>
2014-04-17 19:13:27 +0200

    Syntax fixed

--- wildfly/metadata.rb
+++ wildfly/metadata.rb
@@ -6,5 +6,5 @@ description      "Installs/Configures wildfly"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1
 
-recipe "wildfly::default", "Installs WildFly
+recipe "wildfly::default", "Installs WildFly"
 
