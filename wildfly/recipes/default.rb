4a0148ed7806a8b53b7751a1e580ce0198dc714f
root <root@localhost.localdomain>
2014-04-22 09:38:12 +0200

    wildfly - download and install in /root/temp

--- wildfly/recipes/default.rb
+++ wildfly/recipes/default.rb
@@ -7,10 +7,33 @@
 # All rights reserved - Do Not Redistribute
 #
 
-directory '/root/test' do
-  owner 'root'
-  group 'root'
-  mode 0755
-  recursive true
+
+#Create WildFly System user
+user 'wildfly' do
+  comment 'WildFly'
+  shell '/bin/bash'
+  supports manage_home: true
+  action [:create, :lock]
 end
+
+#Create WildFly Group
+group 'wildfly' do
+  append true
+  members 'wildfly'
+  action :create
+end
+
+# Download WildFly tarball
+remote_file "Download and install WildFly" do
+  source "http://download.jboss.org/wildfly/8.0.0.Final/wildfly-8.0.0.Final.tar.gz"
+  path "/root/temp/wildfly-8.0.0.Final.tar.gz"
+  notifies :run, "execute[Unpack and install WildFly]", :immediately
+end
+
+execute "Unpack and install WildFly" do
+  command "tar -xvzf wildfly-8.0.0.Final.tar.gz"
+  action :run
+  cwd "/root/temp"
+end
+
 
