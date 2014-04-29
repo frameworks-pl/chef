a4b521bea21e62de125d8df7c7dd0dcb431a48b7
root <root@ip-10-105-175-49.eu-west-1.compute.internal>
2014-04-29 16:40:50 +0000

    Added dependency on java cookbook

--- wildfly/metadata.rb
+++ wildfly/metadata.rb
@@ -1,11 +1,12 @@
 name             "wildfly"
-maintainer       "freelancer"
+maintainer       "Janusz Grabis"
 maintainer_email "janusz@rsi.pl"
 license          "All rights reserved"
 description      "Installs/Configures wildfly"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "java", "~> 1.22.0"
 recipe "wildfly::default", "Installs WildFly"
 
 
