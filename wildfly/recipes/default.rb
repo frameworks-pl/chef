2069d5d6c66386e27222727f765226be963eb80f
root <root@ip-10-105-175-49.eu-west-1.compute.internal>
2014-04-29 07:04:47 +0000

    Ownerships set up, startup script in place

--- wildfly/recipes/default.rb
+++ wildfly/recipes/default.rb
@@ -7,33 +7,51 @@
 # All rights reserved - Do Not Redistribute
 #
 
-
 #Create WildFly System user
-user 'wildfly' do
-  comment 'WildFly'
+user 'jboss' do
+  comment 'JBoss'
   shell '/bin/bash'
   supports manage_home: true
   action [:create, :lock]
 end
 
+
 #Create WildFly Group
-group 'wildfly' do
+group 'jboss' do
   append true
-  members 'wildfly'
+  members 'jboss'
   action :create
 end
+
 
 # Download WildFly tarball
 remote_file "Download and install WildFly" do
   source node['wildfly']['url']
-  path "/root/temp/wildfly-8.0.0.Final.tar.gz"
-  notifies :run, "execute[Unpack and install WildFly]", :immediately
+  path "/root/" + node['wildfly']['remote_filename']
+  #notifies :run, "execute[Unpack and install WildFly]", :immediately
 end
 
+
 execute "Unpack and install WildFly" do
-  command "tar -xvzf wildfly-8.0.0.Final.tar.gz -C /opt" 
+  command "tar -xvzf " + node['wildfly']['remote_filename'] + " -C /opt --transform s/" + node['wildfly']['release_name'] + "/wildfly/" 
   action :run
   cwd "/root"
+end
+
+
+execute "Change ownership of wildfly installation" do
+    command "chown jboss:jboss -R /opt/wildfly"
+    action :run
+    cwd '/'
+end
+
+
+#Create startup script
+template File.join('etc', 'init.d', 'wildfly') do
+  source 'wildfly-initd-centos.sh.erb'
+  user 'root'
+  group 'root'
+  mode '0755'
 end
 
 
