c72beba142f0281d874409e8b21b73cbb1260361
Janusz Grabis <janusz@rsi.pl>
2014-04-29 17:49:33 +0200

    Initial version of Java cookbook

---
+++ java/metadata.rb
@@ -0,0 +1,9 @@
+name             "java"
+maintainer       "Janusz Grabis"
+maintainer_email "janusz@rsi.pl"
+license          "All rights reserved"
+description      "Installs Java"
+#long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "java::default", "Installs Java"
