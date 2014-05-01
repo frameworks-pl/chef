8fc67ff569dacd95944489791e36edcb8dd3ec58
root <root@ip-10-105-175-49.eu-west-1.compute.internal>
2014-05-01 05:48:26 +0000

    Added dependency on mysql

--- wildfly/metadata.rb
+++ wildfly/metadata.rb
@@ -7,6 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 depends "java", "~> 1.22.0"
+depends "mysql"
 recipe "wildfly::default", "Installs WildFly"
 
 
