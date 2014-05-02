82423422c69d94392c9bda80d9fcd2c077513bbc
root <root@ip-10-105-175-49.eu-west-1.compute.internal>
2014-05-02 05:28:48 +0000

    iptables commented out

--- wildfly/metadata.rb
+++ wildfly/metadata.rb
@@ -7,7 +7,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 depends "java", "~> 1.22.0"
-depends "mysql"
+#depends "mysql"
+#depends "simple_iptables"
 recipe "wildfly::default", "Installs WildFly"
 
 
